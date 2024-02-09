300de98f76bd7c6d204ae5b6a9443b014f69a2c3
user <user@email.com>
2024-02-09 01:18:38 +0000

    deploy

---
+++ precache-manifest.f932a685a7f9d14d7dfb3e074e2dc1e0.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf27b1d4eabec8852c97d116abb06ec",
+    "url": "/template/404.html"
+  },
+  {
+    "revision": "1b2720b79f41052a1c5d324dfec20d66",
+    "url": "/template/README.md"
+  },
+  {
+    "revision": "4c3b5b1a5b73af09c99d",
+    "url": "/template/css/chunk-vendors.b3ec70fb.css"
+  },
+  {
+    "revision": "5f43cb081d4ae7cda4a5",
+    "url": "/template/css/index.697e3695.css"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/template/fonts/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/template/fonts/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/template/fonts/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/template/fonts/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/template/fonts/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/template/fonts/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "e04efa7527460e6d38b1ba79b405761b",
+    "url": "/template/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.e04efa75.woff"
+  },
+  {
+    "revision": "3e82d9a3cad4ab32b96d5589d55db0e6",
+    "url": "/template/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.3e82d9a3.woff2"
+  },
+  {
+    "revision": "2cd462f936c27120021918882f34d16f",
+    "url": "/template/fonts/gok-H7zzDkdnRel8-DQ6KAXJ69wP1tGnf4ZGhUcY.2cd462f9.woff"
+  },
+  {
+    "revision": "6d72bbd3267f0ab52a76a0c59517c79b",
+    "url": "/template/fonts/gok-H7zzDkdnRel8-DQ6KAXJ69wP1tGnf4ZGhUcel5euIg.6d72bbd3.woff2"
+  },
+  {
+    "revision": "ff076f222c030b7ec40b7372aa008746",
+    "url": "/template/img/splashscreens/ipad_splash.png"
+  },
+  {
+    "revision": "22d271afcd4d6ad3c3ef73ee82a52155",
+    "url": "/template/img/splashscreens/ipadpro1_splash.png"
+  },
+  {
+    "revision": "e306267e1731bf56a99f9628384a72ce",
+    "url": "/template/img/splashscreens/ipadpro2_splash.png"
+  },
+  {
+    "revision": "920d92e685404fac3cd4f8c2081fb62c",
+    "url": "/template/img/splashscreens/ipadpro3_splash.png"
+  },
+  {
+    "revision": "734e1a4b243d49dd3982fb3afe23d6d4",
+    "url": "/template/img/splashscreens/iphone5_splash.png"
+  },
+  {
+    "revision": "0ccb61711de8799d82d2effc316a7a35",
+    "url": "/template/img/splashscreens/iphone6_splash.png"
+  },
+  {
+    "revision": "9dd7c15cec7633e170f21e7d9aefb1a0",
+    "url": "/template/img/splashscreens/iphoneplus_splash.png"
+  },
+  {
+    "revision": "606e7a747b1415c2dca7e1d94027aa4a",
+    "url": "/template/img/splashscreens/iphonex_splash.png"
+  },
+  {
+    "revision": "e507b31be727cd9c77e38ce98212cf27",
+    "url": "/template/img/splashscreens/iphonexr_splash.png"
+  },
+  {
+    "revision": "677c8a04d08e24aa9692bddf8b116115",
+    "url": "/template/img/splashscreens/iphonexsmax_splash.png"
+  },
+  {
+    "revision": "0348a1b9f0c819e6e8a4389bdde215ed",
+    "url": "/template/index.html"
+  },
+  {
+    "revision": "4c3b5b1a5b73af09c99d",
+    "url": "/template/js/chunk-vendors.3491ae56.js"
+  },
+  {
+    "revision": "5f43cb081d4ae7cda4a5",
+    "url": "/template/js/index.bb55ddbc.js"
+  },
+  {
+    "revision": "23a0fb6ab5d6fe56d7b5d49d1b0a0553",
+    "url": "/template/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/template/robots.txt"
+  }
+]);
